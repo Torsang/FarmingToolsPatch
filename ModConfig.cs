4c92a793d3ee85e69c0f11c3d0392e6da61da047
Torsang <Torsang@users.noreply.github.com>
2024-05-16 01:23:43 -0700

    1.1.1
    Added behavior modification for all upgraded tools.
    Finished GMCM functionality to control above-mentioned feature.
    Refactored Harmony method for (hopefully) more reasonable efficiency with expanded features.

--- ModConfig.cs
+++ ModConfig.cs
@@ -8,13 +8,31 @@ namespace FarmingToolsPatch
 {
     public class ModConfig
     {
-        public int bLength, cLength, sLength, gLength, iLength;
-        public int bRadius, cRadius, sRadius, gRadius, iRadius;
+        public int cLength, sLength, gLength, iLength;
+        public int cRadius, sRadius, gRadius, iRadius;
+        public bool cBool, sBool, gBool, iBool;
 
         public ModConfig()
         {
+            //Iridium fields
             iLength = 5;
             iRadius = 2;
+            iBool = true;
+
+            //Gold fields
+            gLength = 6;
+            gRadius = 1;
+            gBool = true;
+
+            //Steel fields
+            sLength = 3;
+            sRadius = 1;
+            sBool = true;
+
+            //Copper fields
+            cLength = 3;
+            cRadius = 0;
+            cBool = true;
         }
     }
 }
--- ModEntry.cs
+++ ModEntry.cs
@@ -7,6 +7,7 @@ using StardewModdingAPI;
 using StardewValley;
 using StardewModdingAPI.Events;
 using GenericModConfigMenu;
+using Microsoft.Xna.Framework.Input;
 
 /***
  ** Original credit to ToweringRedwood for creating this mod in the first place. Many thanks, you're a legend. I hope to do justice to your work.
@@ -29,6 +30,7 @@ namespace FarmingToolsPatch
         {
             config = Helper.ReadConfig<ModConfig>();
             helper.Events.GameLoop.GameLaunched += this.GameLaunched;
+            //helper.Events.Input.ButtonPressed += this.OnButtonPressed;
 
             Harmony harmony = new Harmony("Torsang.PatchedFarmingTools");
             Type[] types = new Type[] { typeof(Vector2), typeof(int), typeof(Farmer) };
@@ -43,7 +45,7 @@ namespace FarmingToolsPatch
 
             if (configMenu is null)
             {
-                this.Monitor.Log("Config Menu is somehow null. Fix it!");
+                Monitor.Log("Config Menu is somehow null. Fix it!");
                 return;
             }
 
@@ -64,9 +66,16 @@ namespace FarmingToolsPatch
             configMenu.AddSectionTitle
             (
                 mod: this.ModManifest, 
-                text: () => "Iridium Tools "
+                text: () => "Iridium Tools"
             );
-
+            configMenu.AddBoolOption
+            (
+                mod: this.ModManifest,
+                name: () => "Modify Iridum Tools",
+                tooltip: () => "Check to use the below settings, uncheck to use vanilla values",
+                getValue: () => config.iBool,
+                setValue: value => config.iBool = value
+            );
             configMenu.AddNumberOption
             (
                 mod: this.ModManifest,
@@ -77,7 +86,6 @@ namespace FarmingToolsPatch
                 min: 0,
                 interval: 1
             );
-
             configMenu.AddNumberOption
             (
                 mod: this.ModManifest,
@@ -88,18 +96,141 @@ namespace FarmingToolsPatch
                 min: 0,
                 interval: 1
             );
-        }
-    }
 
-    class PatchedFarmingTilesAffected
-    {
-        static public void Postfix(ref List<Vector2> __result, Vector2 tileLocation, int power, Farmer who)
+            configMenu.AddSectionTitle
+            (
+                mod: this.ModManifest,
+                text: () => "Gold Tools"
+            );
+            configMenu.AddBoolOption
+            (
+                mod: this.ModManifest,
+                name: () => "Modify Gold Tools",
+                tooltip: () => "Check to use the below settings, uncheck to use vanilla values",
+                getValue: () => config.gBool,
+                setValue: value => config.gBool = value
+            );
+            configMenu.AddNumberOption
+            (
+                mod: this.ModManifest,
+                name: () => "Length",
+                tooltip: () => "Number of tiles away the tool will reach",
+                getValue: () => config.gLength,
+                setValue: value => config.gLength = value,
+                min: 0,
+                interval: 1
+            );
+            configMenu.AddNumberOption
+            (
+                mod: this.ModManifest,
+                name: () => "Radius",
+                tooltip: () => "Number of parallel lines on each side of the tool's reach",
+                getValue: () => config.gRadius,
+                setValue: value => config.gRadius = value,
+                min: 0,
+                interval: 1
+            );
+
+            configMenu.AddSectionTitle
+            (
+                mod: this.ModManifest,
+                text: () => "Steel Tools"
+            );
+            configMenu.AddBoolOption
+            (
+                mod: this.ModManifest,
+                name: () => "Modify Steel Tools",
+                tooltip: () => "Check to use the below settings, uncheck to use vanilla values",
+                getValue: () => config.sBool,
+                setValue: value => config.sBool = value
+            );
+            configMenu.AddNumberOption
+            (
+                mod: this.ModManifest,
+                name: () => "Length",
+                tooltip: () => "Number of tiles away the tool will reach",
+                getValue: () => config.sLength,
+                setValue: value => config.sLength = value,
+                min: 0,
+                interval: 1
+            );
+            configMenu.AddNumberOption
+            (
+                mod: this.ModManifest,
+                name: () => "Radius",
+                tooltip: () => "Number of parallel lines on each side of the tool's reach",
+                getValue: () => config.sRadius,
+                setValue: value => config.sRadius = value,
+                min: 0,
+                interval: 1
+            );
+
+            configMenu.AddSectionTitle
+            (
+                mod: this.ModManifest,
+                text: () => "Copper Tools"
+            );
+            configMenu.AddBoolOption
+            (
+                mod: this.ModManifest,
+                name: () => "Modify Copper Tools",
+                tooltip: () => "Check to use the below settings, uncheck to use vanilla values",
+                getValue: () => config.cBool,
+                setValue: value => config.cBool = value
+            );
+            configMenu.AddNumberOption
+            (
+                mod: this.ModManifest,
+                name: () => "Length",
+                tooltip: () => "Number of tiles away the tool will reach",
+                getValue: () => config.cLength,
+                setValue: value => config.cLength = value,
+                min: 0,
+                interval: 1
+            );
+            configMenu.AddNumberOption
+            (
+                mod: this.ModManifest,
+                name: () => "Radius",
+                tooltip: () => "Number of parallel lines on each side of the tool's reach",
+                getValue: () => config.cRadius,
+                setValue: value => config.cRadius = value,
+                min: 0,
+                interval: 1
+            );
+        }
+
+        /* Added primarily for debugging purposes
+         * TODO: Find out how to detect the reach enchant so I can work with it later
+        */
+        /*private void OnButtonPressed(object sender, ButtonPressedEventArgs e)
         {
-            if (power == 5)
+            // ignore if player hasn't loaded a save yet
+            if (!Context.IsWorldReady)
+                return;
+
+            // print button presses to the console window
+            if (e.Button is SButton.MouseLeft)
+            {
+                this.Monitor.Log($"Player has {Game1.player.enchantments.Count} enchantments.", LogLevel.Debug);
+                if (Game1.player.enchantments.Count > 0)
+                {
+                    foreach (BaseEnchantment ench in Game1.player.enchantments)
+                    {
+                        this.Monitor.Log($"Player has the following enchantments: {ench}.", LogLevel.Debug);
+                    }
+                }
+            }
+        }*/
+
+        class PatchedFarmingTilesAffected
+        {
+            static public void Postfix(ref List<Vector2> __result, Vector2 tileLocation, int power, Farmer who)
             {
                 __result.Clear();
                 Vector2 direction;
                 Vector2 orthogonal;
+                int length = 1, radius = 0;
 
                 switch (who.FacingDirection)
                 {
@@ -120,8 +251,33 @@ namespace FarmingToolsPatch
                         break;
                 }
 
-                int length = ModEntry.config.iLength;
-                int radius = ModEntry.config.iRadius;
+                //Set copper values, modded or vanilla
+                if (power >= 2)
+                {
+                    length = (ModEntry.config.cBool) ? ModEntry.config.cLength : 3;
+                    radius = (ModEntry.config.cBool) ? ModEntry.config.cRadius : 0;
+                }
+
+                //Set steel values, modded or vanilla
+                if (power >= 3)
+                {
+                    length = (ModEntry.config.sBool) ? ModEntry.config.sLength : 5;
+                    radius = (ModEntry.config.sBool) ? ModEntry.config.sRadius : 0;
+                }
+
+                //Set gold values, modded or vanilla
+                if (power >= 4)
+                {
+                    length = (ModEntry.config.gBool) ? ModEntry.config.gLength : 3;
+                    radius = (ModEntry.config.gBool) ? ModEntry.config.gRadius : 1;
+                }
+
+                //Set iridium values, modded or vanilla
+                if (power >= 5)
+                {
+                    length = (ModEntry.config.iBool) ? ModEntry.config.iLength : 6;
+                    radius = (ModEntry.config.iBool) ? ModEntry.config.iRadius : 1;
+                }
 
                 for (int x = 0; x < length; x++)
                 {
